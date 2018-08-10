a061e4d8ef4f7e6960a8bae8e01ad9d837df6a2e
Unknown <grizzly.hok@gmail.com>
2018-08-10 08:28:07 +0200

    Player class

--- card_game.py
+++ card_game.py
@@ -45,6 +45,28 @@ class Hand:
         return self.cards.pop()
 
 
+class Player:
+
+    def __init__(self, name, hand):
+        self.name = name
+        self.hand = hand
+
+    def play_card(self):
+        drawn_card = self.hand.remove_card()
+        print("{} has placed: {}".format(self.name, drawn_card))
+        print("\n")
+        return drawn_card
+
+    def remove_war_cards(self):
+        war_cards = []
+        for x in range(3):
+            war_cards.append(self.hand.remove_card())
+        return war_cards
+
+    def still_has_cards(self):
+        return len(self.hand.cards) != 0
+
+
 start = "Welcome to War, let's begin..."
 
 print(start)
